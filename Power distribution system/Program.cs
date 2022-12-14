2429a9f288586844e877a2910e7eb88a6266fcc0
Knoxianes <strahinja2001@gmail.com>
2022-12-14 23:05:19 +0100

    Test

--- Power distribution system/Program.cs
+++ Power distribution system/Program.cs
@@ -6,7 +6,7 @@ namespace Power_distribution_system
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
         }
     }
 }

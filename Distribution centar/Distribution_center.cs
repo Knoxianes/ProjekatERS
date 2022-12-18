2fe26b35b74ed0794bc7b3e281a21af2de26f2f0
natasaam <natasamilanovic333@gmail.com>
2022-12-18 13:42:16 +0100

    logFIle

--- Distribution centar/Distribution_center.cs
+++ Distribution centar/Distribution_center.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Distribution_centar
 {
@@ -55,7 +56,7 @@ namespace Distribution_centar
         {
             try
             {
-                var task_consumer = await Server_Recieve_Consumer( 8000);
+                var task_consumer = await Server_Recieve_Consumer(8000);
                 var task_powerplant = await Server_Recieve_Powerplant(8001);
                 var task_solar_wind = await Server_Recieve_Solar_wind(8002);
                 Task.WaitAll(task_consumer, task_powerplant, task_solar_wind);
@@ -123,7 +124,7 @@ namespace Distribution_centar
                             last_received_message_powerplant = Encoding.ASCII.GetString(buffer, 0, buffer.Length); //Dekodiranje poruke
                             Console.WriteLine(last_received_message_powerplant);
                         }
-                        catch(Exception e)
+                        catch (Exception e)
                         {
                             Console.WriteLine("Greska prilikom prijema poruke: " + e);
                             Environment.Exit(2);
@@ -186,10 +187,22 @@ namespace Distribution_centar
                 "\n\tTrenutno stanje u celoj mrezi:\n" +
                 "\tPotrebno energije: " + Potrebno_energije + "\n" +
                 "\tTrenutna prozivodnja: " + trenutna_proizvodnja +
-                solar_wind.ToString()+
-                powerplant.ToString()+
-                izvestaj.ToString()+
+                solar_wind.ToString() +
+                powerplant.ToString() +
+                izvestaj.ToString() +
                 "\n_______________\n";
+        }
+
+        public void LogFile(string tekst)
+        {
+            //otvara fajl za pisanje i stavlja u promenljivu file
+            StreamWriter file = new StreamWriter("log.txt", true);          //Ako zelimo da zapisujemo dogadjaje u fajl u realnom vremenu, mozemo otvoriti fajl pri svakom pozivu i zatvoriti ga nakon sto zapisemo dogadjaj.
+
+            //zapisuje liniju teksta u fajl 
+            file.WriteLine(tekst);
+
+            //zatvaranje fajla
+            file.Close(); 
         }
     }
 }

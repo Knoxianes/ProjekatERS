2f276ee5786dfd779dd45e8ad25b645ac766bc28
natasaam <natasamilanovic333@gmail.com>
2023-01-08 20:10:47 +0100

    Testovi 2. dio

--- PowerDistributionSystemTests/IzvestajTest.cs
+++ PowerDistributionSystemTests/IzvestajTest.cs
@@ -8,5 +8,70 @@ using System.Threading.Tasks;
 
 namespace PowerDistributionSystemTests
 {
-   
+    [TestFixture]
+
+    public class IzvestajTest
+    {
+
+        [Test]
+        public void Add_AddItem() 
+        {
+            //Arrange
+            Izvestaj izvestaj = new Izvestaj();
+            int vati = 1;
+            int cena = 1;
+
+
+            //Act
+            izvestaj.Add(vati, cena);
+
+            //Assert
+            CollectionAssert.IsNotEmpty(izvestaj.Potrosaci);
+        
+        }
+
+
+        [Test]
+        public void Remove_RemovesItem()
+        {
+            //Arrange
+            Izvestaj izvestaj = new Izvestaj();
+            int vati = 1;
+            int cena = 1;
+            Potrosac tmp = new Potrosac(vati, cena);
+            int id = 1;
+            tmp.Id = id;
+            izvestaj.Potrosaci.Add(tmp);
+
+
+            //Act
+            izvestaj.Remove(id);
+
+            //Assert
+            CollectionAssert.IsEmpty(izvestaj.Potrosaci);
+        }
+
+        [Test]
+        public void Izracunaj_Izvestaj_ReturnsTrue()
+        {
+            //Arrange
+            Izvestaj i = new Izvestaj();
+
+            int vati = 1;
+            int cena = 1;
+            Potrosac tmp = new Potrosac(vati, cena);
+            tmp.Id = 1;
+            i.Potrosaci.Add(tmp);
+            i.izvestaj.Add(tmp.Id, 1);
+
+            //Act
+            bool result = i.Izracunaj_izvestaj();
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+       
+
+    }
 }
